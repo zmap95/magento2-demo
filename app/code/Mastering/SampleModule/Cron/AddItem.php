18b0f1c4927a123dea46aac307e040a777dcdadd
BigCandy <hoangcanhit95@gmail.com>
2021-06-16 22:45:49 +0700

    System config

--- app/code/Mastering/SampleModule/Cron/AddItem.php
+++ app/code/Mastering/SampleModule/Cron/AddItem.php
@@ -4,18 +4,23 @@ namespace Mastering\SampleModule\Cron;
 
 use Magento\Framework\App\Cron;
 use Mastering\SampleModule\Model\ItemFactory;
+use Mastering\SampleModule\Model\Config;
 
 class AddItem
 {
     protected $itemFactory;
+    private $config;
 
-    public function __construct(ItemFactory $itemFactory)
+    public function __construct(ItemFactory $itemFactory, Config $config)
     {
         $this->itemFactory = $itemFactory;
+        $this->config = $config;
     }
 
     public function execute()
     {
-        $this->itemFactory->create()->setName('Schedule at' . time())->save();
+        if ($this->config->isEnabled()) {
+            $this->itemFactory->create()->setName('Schedule at' . time())->save();
+        }
     }
 }
