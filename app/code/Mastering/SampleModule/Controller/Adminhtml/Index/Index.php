ff2ba0248f3035d9941e8f9f1b22e9c3ae5888d4
BigCandy <hoangcanhit95@gmail.com>
2021-06-24 11:40:20 +0700

    Admin Grid

--- app/code/Mastering/SampleModule/Controller/Adminhtml/Index/Index.php
+++ app/code/Mastering/SampleModule/Controller/Adminhtml/Index/Index.php
@@ -2,7 +2,7 @@
 
 namespace Mastering\SampleModule\Controller\Adminhtml\Index;
 
-use Magento\Framework\App\Action\Action;
+use Magento\Backend\App\Action;
 use Magento\Framework\Controller\Result\Raw;
 use Magento\Framework\Controller\ResultFactory;
 
@@ -10,9 +10,6 @@ class Index extends Action
 {
     public function execute()
     {
-        /** @var Raw $result */
-        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-
-        return $result->setContents('Hello Admin');
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
---
+++ app/code/Mastering/SampleModule/Model/ResourceModel/Item/Grid/Collection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mastering\SampleModule\Model\ResourceModel\Item\Grid;
+
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+use Mastering\SampleModule\Model\ResourceModel\Item\ItemResource;
+use Psr\Log\LoggerInterface as Logger;
+
+class Collection extends SearchResult
+{
+    public function __construct(
+        EntityFactory $entityFactory,
+        Logger $logger,
+        FetchStrategy $fetchStrategy,
+        EventManager $eventManager,
+        $mainTable = 'mastering_sample_item',
+        $resourceModel = 'Mastering\SampleModule\Model\ResourceModel\Item',
+        $identifierName = null,
+        $connectionName = null
+    ) {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $mainTable, $resourceModel, $identifierName, $connectionName);
+    }
+}
